585ebfb5b54c9f29b45ae06665d81b7bb3155b5b
kaynekaynekayne <theygoon@gmail.com>
2023-06-16 03:09:19 +0900

    3-2-class

--- 3-1-without-oop.ts
+++ 3-1-without-oop.ts
@@ -5,21 +5,24 @@
         hasMilk:boolean;
     }
 
-    const BEANS_GRAM_PER_SHOT=7;
-    let coffeeBeans:number=0;
+    const BEANS_GRAM_PER_SHOT=7; //1샷 당 필요한 커피콩 7g
+    let coffeeBeans:number=0; //현재 커피콩 0g
 
     function makeCoffee(shots:number):CoffeeCup{
         if(coffeeBeans<shots*BEANS_GRAM_PER_SHOT){
+            //총 커피콩이 샷*7g(1샷 당 필요한 커피콩)보다 작다면
+            //예를 들어 2샷이면 2*7g이라 총 커피콩이 14g이 필요함
             throw new Error('Not enough coffee beans');
         } 
-        coffeeBeans-=shots*BEANS_GRAM_PER_SHOT;
+        coffeeBeans-=shots*BEANS_GRAM_PER_SHOT; 
+        //커피 한 번 내리면 총 커피콩(g)은 샷*7g만큼 줄어듦
         return {
             shots,
-            hasMilk:false,
+            hasMilk:false, //샷과 우유포함 여부 보내줌
         }
     }
 
-    coffeeBeans+=3*BEANS_GRAM_PER_SHOT;
-    const coffee=makeCoffee(2);
+    coffeeBeans+=3*BEANS_GRAM_PER_SHOT; //일단 총 커피콩 21g으로 충전해줌
+    const coffee=makeCoffee(2); //2샷을 넣겠다
     console.log(coffee);
 }
---
+++ 3-2-class.ts
@@ -0,0 +1,45 @@
+{
+    //coffeeMachine class 안에 coffeeBeans / makeCoffee()
+    
+    type CoffeeCup={
+        shots:number;
+        hasMilk:boolean;
+    }
+
+    class coffeeMachine{
+        coffeeBeans:number=0; //"instance level"
+        static BEANS_GRAM_PER_SHOT:number=7; 
+        //static 때문에 "class level"로 지정이 됨
+        //클래스와 연결이 되어있기 때문에 object마다 만들어지지 않음
+        //console.log(new coffeeMachine()) => {coffeeBeans:32} 빈그램퍼샷 숨김
+        
+        constructor(coffeeBeans:number){
+            //constructor:클래스를 이용해서 obj를 만들 때 처음에 호출되는 함수
+            this.coffeeBeans=coffeeBeans;
+        }
+
+        static makeMachine(coffeeBeans:number):coffeeMachine{
+            return new coffeeMachine(coffeeBeans)
+        }
+
+        makeCoffee(shots:number):CoffeeCup{
+            if(this.coffeeBeans<shots*coffeeMachine.BEANS_GRAM_PER_SHOT){
+                throw new Error('Not enough coffee beans')
+            }
+            this.coffeeBeans-=shots*coffeeMachine.BEANS_GRAM_PER_SHOT;
+            return {
+                shots,
+                hasMilk:false
+            }
+        }
+
+    }
+
+    const maker=new coffeeMachine(32);
+    console.log(maker);
+    //static 붙이기 전엔 { coffeeBeans: 32, BEANS_GRAM_PER_SHOT: 7 }
+    console.log(maker.makeCoffee(2));
+    //{ shots: 2, hasMilk: false }
+
+    console.log(coffeeMachine.makeMachine(3));
+}
